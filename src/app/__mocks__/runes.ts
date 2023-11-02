5779d0482c6b4227f87fc3cb99e3f4e599ee20be
Geovana Ribeiro <geovana.ribeiro@f1rst.com>
2023-11-02 03:54:30 -0300

    create a array mock of runes to be displayed in view

---
+++ src/app/__mocks__/runes.ts
@@ -0,0 +1,166 @@
+import { Rune } from "../interfaces/rune.interface";
+
+export const runes: Array<Rune> = [
+  {
+    symbol: `K` ,
+    name: 'Ratanen',
+    title: 'Revela meu potencial. Sou realizador(a) de sonhos.',
+    description: 'Ratanen representa as névoas do passado, presente e futuro. Fala sobre o conhecimento de criar ou destruir as coisas, sobre o caos e sobre o tempo. Pode demonstrar sonhos, pois Ratanen é feito da mesma substância dos mesmos. Também demonstra a força que impede o tempo de estagnar e de esvair.'
+  },
+  {
+    symbol: `D` ,
+    name: 'Drakani',
+    title: 'Reforça a minha voontade; Sou soberania.',
+    description: 'É a runa de Tiamat, a Deusa-mãe de todos os Dragões, e também aquela cujo corpo criou a terra e os céus. Ela pode modificar o caos, dando-lhe forma, bem como pode criar o tempo por si mesmo e também aqueles que o habitam. Drakani tem domínio sobre Ratanen, assim como para obter respostas do caos, ou auxílio de Tiamat.'
+  },
+  {
+    symbol: `S` ,
+    name: `Sha'oren`,
+    title: 'Revela meu conhecimento; Sou um buscador',
+    description: `A runa do éter, que contém todo o conhecimento. O entendimento sobre esta runa vem com o tempo, mas saiba que o conhecimento puro pode ser prejudicial sem a sabedoria para utilizá-lo. Pode ser combinado com Drakani para entender o caos entre as névoas. Meditar com Sha'oren pode trazer o conhecimento puro provindo do éter e do Akasha.`
+  },
+  {
+    symbol: `I` ,
+    name: 'Ihanen',
+    title: 'Expande minha força criativa; Sou inspiração.',
+    description: 'Representa o ato de vagar pelo Caos e recolher matéria onírica, usando-a para moldar o mundo.'
+  },
+  {
+    symbol: 'F' ,
+    name: 'Ma’erhen',
+    title: 'Reflete meu espírito; Sou unidade.',
+    description: 'Runa do espírito e dos Dragões Astrais que existem nos planos superiores.'
+  },
+  {
+    symbol: 'W' ,
+    name: 'Ra',
+    title: 'Revela meus fluxos; sou paciência.',
+    description: 'Representa a cura e o conhecimento profundo guiado pela curiosidade.'
+  },
+  {
+    symbol: 'M' ,
+    name: 'Liwaen',
+    title: 'A paz da minha sombra; Sou sutileza.',
+    description: 'Dragões da escuridão, regendo a contemplação e a proteção; o recolhimento ao inconsciente para compreensão da própria essência.'
+  },
+  {
+    symbol: 'H' ,
+    name: 'Li',
+    title: 'Acende minha paixão; Sou fogo, chama e luz.',
+    description: 'Dragões do fogo vermelho, que regem os estados mentais de paixão, fúria e lealdade.'
+  },
+  {
+    symbol: 'T' ,
+    name: 'Li’aan',
+    title: 'Molda a minha percepção; Sou revelação.',
+    description: 'Dragões da luz, que podem iluminar assuntos ocultos, descobrir segredos e promover a justiça, com clareza de visão.'
+  },
+  {
+    symbol: 'R' ,
+    name: 'Mah',
+    title: 'Ressoa minha voz; sou autoridade.',
+    description: 'Dragões do trovão, regendo a sagacidade e a interação social, além do carisma e da empatia.'
+  },
+  {
+    symbol: 'N' ,
+    name: 'Ninathan',
+    title: 'Me traz saúde; Sou parte da natureza.',
+    description: 'Dragões das florestas associado ao crescimento, à fertilidade, à fauna e à flora, e à vida de forma geral.'
+  },
+  {
+    symbol: 'TH' ,
+    name: 'Thaan',
+    title: 'Expressa minha força. Sou poder.',
+    description: 'Dragões brancos das montanhas que rege o espírito de equipe, a harmonia entre povos e o conhecimento forjado com base na união de uma sociedade.'
+  },
+  {
+    symbol: 'P' ,
+    name: 'Waethan',
+    title: 'Mostra meu foco; Sou trabalhador(a).',
+    description: 'Dragões de cristal que regem  às ideias e aos pensamentos manifestados de forma organizada e magistral'
+  },
+  {
+    symbol: 'O' ,
+    name: 'Ta’kaya ',
+    title: 'Me ensina a fluir; Sou adaptabilidade.',
+    description: 'Representa os ciclos da natureza, a forma que as coisas ocorrem de ano a ano e assim se repetem.'
+  },
+  {
+    symbol: 'SH' ,
+    name: 'Shaan',
+    title: 'Ressoa minha energia pessoal; Sou moldador(a).',
+    description: 'Representa o aprendizado do Mago a controlar o Caos, moldando-o à sua vontade. É a runa da magia, com fortes energias que requerem direcionamento.'
+  },
+  {
+    symbol: 'NG' ,
+    name: 'Deigomah',
+    title: 'Revela minhas feridas; Sou curador(a).',
+    description: 'Representa a magia em situações caóticas que acabam gerando mais caos.'
+  },
+  {
+    symbol: 'A' ,
+    name: 'Athihan',
+    title: 'Destrói minhas fraquezas; Sou guerreiro(a).',
+    description: 'Athihan é a runa das guerras e dos conflitos necessários para esculpir e moldar as civilizações de forma que elas possam finalmente prosperar e se manter.'
+  },
+  {
+    symbol: 'B' ,
+    name: 'Kaegos',
+    title: 'Me traz equilíbrio; Sou simétrico(a).',
+    description: 'Runa do equilíbrio que surge das cinzas dos conflitos presenciados em Athihan.'
+  },
+  {
+    symbol: 'Z' ,
+    name: 'Thaenrathi',
+    title: 'Me ensina liderança; Sou um farol.',
+    description: 'A runa do campeão que governa de forma justa.'
+  },
+  {
+    symbol: 'E' ,
+    name: 'Athanen',
+    title: 'Ensina através dos meus hábitos; Sou ordem.',
+    description: 'A runa da ordem e da lei, onde pode-se prosperar em tempos de justiça, paz e calmaria.'
+  },
+  {
+    symbol: 'Y' ,
+    name: 'Shoren',
+    title: 'Revela minha sabedoria; Sou sábio(a)',
+    description: 'A runa da sabedoria, que pode ser obtida através do equilíbrio e justiça, possibilitando reflexões filosóficas e o entendimento do mundo e de si mesmo.'
+  },
+  {
+    symbol: 'L' ,
+    name: 'Nanen',
+    title: 'Vibra meus amores; Sou amor.',
+    description: 'A runa da união formada por meio do desenvolvimento de caminhos conjuntos.'
+  },
+  {
+    symbol: 'EI' ,
+    name: 'Ionali',
+    title: 'Revela meus talentos; Sou habilidoso(a).',
+    description: 'Representa a paixão temperada com sabedoria ao entrar em introspecção para experienciar suas motivações na vida, os propósitos que desejam alcançar.'
+  },
+  {
+    symbol: 'V' ,
+    name: 'Drakaneal',
+    title: 'Revela a grande teia. Sou o tecelão(a).',
+    description: ' É a runa da união, da família e da comunidade.'
+  },
+  {
+    symbol: 'J' ,
+    name: 'Zhukaya',
+    title: 'Reflete o meu senso do justiça; Sou honrado(a).',
+    description: 'É a runa da justiça que leva embora todos os problemas e conflitos pendentes, para bem ou para mal. As consequências das ações de todos retornarão para os mesmos, as lições serão ensinadas e recompensas serão dadas.'
+  },
+  {
+    symbol: 'G' ,
+    name: 'Abanen',
+    title: 'Me ilumina com autconsciência; EU SOU...',
+    description: 'Representa a progressão para um novo estágio. É uma época de nostalgia, de plenitude e graça.'
+  },
+  {
+    symbol: 'U' ,
+    name: 'Satihan',
+    title: 'Me ensina a deixar ir embora; Sou recomeço.',
+    description: 'Representa o caos que consome a ordem e se deixa fluir, para que a ordem possa ser refeita. É a runa da destruição e da reconstrução, do recomeço e da renovação.'
+  }
+]
